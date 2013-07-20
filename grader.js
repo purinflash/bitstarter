7b534d22208fb100dff7088cdfd30bc6285ceda6
John Wong <purinflash@mac.com>
2013-07-20 14:05:25 -0600

    hw4

--- grader.js
+++ grader.js
@@ -22,8 +22,11 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,14 +58,34 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHTMLString = function(htmlstring, checksfile) {
+    $ = cheerio.load(htmlstring);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('--url <url>', 'Use URL instead')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (typeof program.url !== "undefined") {
+        rest.get(program.url).on('complete', function(data) {
+  	   var checkJson = checkHTMLString(data,program.checks);
+	   var outJson = JSON.stringify(checkJson, null, 4);
+	   console.log(outJson);
+	});
+    } else {
+    	var checkJson = checkHtmlFile(program.file, program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
